744e102a9c959da3a9296b3c899602f62844fbf0
Prakash Pandey <prakash.pandey01@nagarro.com>
2025-03-24 19:02:23 +0530

    commit login folder

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
