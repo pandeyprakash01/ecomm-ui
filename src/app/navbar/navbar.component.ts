b13c476d24b7104a73cff3d727797ebab7c357da
Prakash Pandey <prakash.pandey01@nagarro.com>
2025-03-26 18:33:42 +0530

    url changed for cognito

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -21,7 +21,7 @@ export class NavbarComponent implements OnInit{
   
   
   constructor(private route: ActivatedRoute,private apiService:ApiService){
-    this.cognitoLoginUrl = "https://ap-south-14laopcgtk.auth.ap-south-1.amazoncognito.com/login?client_id=2onbl9d9ekqmspnhhp47asejmq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fd84l1y8p4kdic.cloudfront.net";
+    this.cognitoLoginUrl = "https://ap-south-14laopcgtk.auth.ap-south-1.amazoncognito.com/login?client_id=2onbl9d9ekqmspnhhp47asejmq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fap-south-14laopcgtk.auth.ap-south-1.amazoncognito.com%2Fcallback";
   }
 
    ngOnInit() {
